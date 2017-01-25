97fb0b7767021d27913b3a0671add572a56dc238
chxuan <787280310@qq.com>
2017-01-25 12:03:28 +0800

    update

--- test/sync_rpc_client/main.cpp
+++ test/sync_rpc_client/main.cpp
@@ -15,15 +15,15 @@ int main()
         auto in_message = std::make_shared<request_person_info_message>();
         in_message->set_name("Jack");
         in_message->set_age(20);
-        auto out_message = client.call("request_person_info", in_message);
-        if (IS_SAME(out_message, response_person_info_message))
+        auto ret_message = client.call("request_person_info", in_message);
+        if (IS_SAME(ret_message, response_person_info_message))
         {
-            auto message = std::dynamic_pointer_cast<response_person_info_message>(out_message); 
+            auto message = std::dynamic_pointer_cast<response_person_info_message>(ret_message); 
             message->PrintDebugString();
         }
-        else if (IS_SAME(out_message, response_error))
+        else if (IS_SAME(ret_message, response_error))
         {
-            auto message = std::dynamic_pointer_cast<response_error>(out_message); 
+            auto message = std::dynamic_pointer_cast<response_error>(ret_message); 
             message->PrintDebugString();
         }
     }
