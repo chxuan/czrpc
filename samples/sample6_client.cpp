a413be509934836deb4b0003532a575b5be7f2ab
chxuan <787280310@qq.com>
2017-03-05 17:01:52 +0800

    update

---
+++ samples/sample6_client.cpp
@@ -0,0 +1,29 @@
+/************************************************
+ * rpc客户端的原始调用接口
+************************************************/
+#include <iostream>
+#include "czrpc/client/client.hpp"
+using namespace czrpc::base;
+
+int main()
+{   
+    // 1.创建rpc客户端对象
+    czrpc::client::rpc_client client;
+    try
+    {
+        // 2.配置连接参数并启动事件循环（非阻塞）
+        client.connect({ "127.0.0.1", 50051 }).run();
+
+        // 3.同步调用echo函数
+        std::string rsp = client.call_raw("echo", "Hello czrpc");
+        std::cout << rsp << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        log_warn(e.what());
+        return 0;
+    }
+
+    std::cin.get();
+    return 0;
+}
---
+++ samples/sample6_server.cpp
@@ -0,0 +1,35 @@
+/************************************************
+ * rpc服务端的原始绑定接口
+************************************************/
+#include <iostream>
+#include "czrpc/server/server.hpp"
+using namespace czrpc::base;
+
+std::string echo(const std::string& req)
+{
+    std::cout << req << std::endl;
+    return req;
+}
+
+int main()
+{
+    // 1.创建rpc服务器对象
+    czrpc::server::server server;
+    try
+    {
+        // 2.绑定echo函数
+        server.bind_raw("echo", &echo);
+
+        // 3.配置监听参数并启动事件循环（非阻塞）
+        // 服务端默认启动一个ios线程和一个work线程
+        server.listen({ "127.0.0.1", 50051 }).run();
+    }
+    catch (std::exception& e)
+    {
+        log_warn(e.what());
+        return 0;
+    }
+
+    std::cin.get();
+    return 0;
+}
